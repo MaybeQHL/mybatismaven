b39263a85073d0162bb5adee92272b3d189124f7
maybe <1013480204@qq.com>
2018-06-27 12:46:19 +0800

    new

--- src/main/java/cn/maybe/pojo/IUser.java
+++ src/main/java/cn/maybe/pojo/IUser.java
@@ -1,6 +1,13 @@
 package cn.maybe.pojo;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface IUser {
 
-    public  void findById();
+    public  User findById();
+    public   List<User> findUserListById();
+    public   List<User> findByUserName(@Param(value="username")String username);
+
 }
--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class TestMain {
 
@@ -21,11 +22,37 @@ public class TestMain {
         Reader reader = null;
         try {
             reader = Resources.getResourceAsReader(resource);
+            //创建会话工厂，传入mybatis的配置信息
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            //通过工厂得到SqlSession
             SqlSession session = sqlSessionFactory.openSession();
+            //通过SqlSession来操作数据库
+            //List中的User泛型，和resultType中指定的类型一致
+           //查询
             User user = session.selectOne("findById",1);
+            List<User> userList = session.selectList("findUserListById",1);
+            //查询(动态sql测试)
+            List<User> userList2=session.selectList("findByUserName","张三");
+            for (User item:
+                   userList2) {
+                System.out.println("动态"+item.getUsername());
+            }
+           //添加
+            User  userNew=new User();
+            userNew.setUsername("asd");
+            userNew.setSex(1);
+            session.insert("insertUser",userNew);
+            System.out.println(userNew.getId());
+           //删除
+            session.delete("deleteUser",userNew.getId());
+            //提交事务
             session.commit();
-            System.out.println(user.getUsername());
+            //释放资源
+            session.close();
+//            for (User item:userList) {
+//                System.out.println(item.getUsername());
+//
+//            }
         }catch (IOException e){
             e.printStackTrace();
         }
